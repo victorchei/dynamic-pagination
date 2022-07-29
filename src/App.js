649d005896ffe4730604c4cef2387b012abeea71
Viktor Zhelizko <viktor.zhelizko@infopulse.com>
2022-07-29 08:42:33 +0300

    add logic for scroll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [fetching, setFetching] = useState(true);
+  const [totalCount, setTotalCount] = useState(0);
+
+  useEffect(() => {
+    if (fetching) {
+      axios.get(`https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${currentPage}`)
+        .then(response => {
+          setPhotos([...photos, ...response.data]);
+          setCurrentPage(prevPage => prevPage + 1);
+          setTotalCount(response.headers['x-total-count']);
+        })
+        .finally(() => setFetching(false))
+    }   
+  }, [fetching]);
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+    return function () {
+      document.removeEventListener('scroll', scrollHandler);
+    }
+  })
+
+  const scrollHandler = (event) => {
+    const { documentElement: el } = event.target;
+    console.log(el.scrollHeight - (el.scrollTop + window.innerHeight) < 100, photos.length, totalCount);
+    if (el.scrollHeight - (el.scrollTop + window.innerHeight) < 100 && photos.length < totalCount) {
+      
+      setFetching(true);
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        {photos.map(photo =>
+          <div className="photo" key={photo.id}>
+            <div className="title">{photo.id}. { photo.title}</div>
+            <img src={photo.thumbnailUrl} alt="" />
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
-export default App;
+export default App
